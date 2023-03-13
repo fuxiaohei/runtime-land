6812d74c50f201bae077b252beb81ba4a9a7ea2a
fuxiaohei <fuxiaohei@vip.qq.com>
2023-03-13 18:12:06 +0800

    feat: begin backend

---
+++ moni-backend/src/config.rs
@@ -0,0 +1,29 @@
+use anyhow::Result;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct HttpConfig {
+    pub addr: String,
+}
+
+impl Default for HttpConfig {
+    fn default() -> Self {
+        Self {
+            addr: "127.0.0.1:8779".to_string(),
+        }
+    }
+}
+
+#[derive(Debug, Default, Serialize, Deserialize)]
+pub struct Config {
+    pub http: HttpConfig,
+}
+
+impl Config {
+    /// read config from toml file
+    pub fn from_file(path: &str) -> Result<Self> {
+        let content = std::fs::read_to_string(path)?;
+        let config: Self = toml::from_str(&content)?;
+        Ok(config)
+    }
+}
---
+++ moni-backend/src/main.rs
@@ -0,0 +1,33 @@
+use clap::Parser;
+use tracing::{debug, warn};
+
+mod config;
+
+#[derive(Parser, Debug)]
+#[clap(name = "moni-backend", version = moni_core::get_version())]
+struct Cli {
+    /// The conf file
+    #[clap(long, default_value("moni-backend.toml"))]
+    pub conf: Option<String>,
+}
+
+#[tokio::main]
+async fn main() {
+    moni_core::init_tracing();
+
+    let args = Cli::parse();
+    let conf_file = args.conf.as_ref().unwrap();
+    // if conf_file is not exist, warn and exit
+    if !std::path::Path::new(conf_file).exists() {
+        warn!("conf file {} is not exist", conf_file);
+        std::process::exit(1);
+    }
+
+    let conf = config::Config::from_file(&conf_file).unwrap();
+    debug!("load conf: {:?}", conf);
+
+    // start rpc server
+    moni_core::rpc::start_server(conf.http.addr.parse().unwrap())
+        .await
+        .unwrap();
+}
