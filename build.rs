4e1f5723be4a528ddba720a9e3aa7861d67b5f4a
fuxiaohei <fuxiaohei@vip.qq.com>
2023-06-25 10:04:27 +0800

    bumps: update cargo deps

--- build.rs
+++ build.rs
@@ -6,6 +6,7 @@ fn main() {
     println!("cargo:rerun-if-changed=wit/*.wit");
 
     build_wit_guest_code();
+    copy_guest_code_to_crates();
 }
 
 fn build_wit_guest_code() {
@@ -25,5 +26,19 @@ fn build_wit_guest_code() {
             let target_rs = wit_dir.join(Path::new(name));
             std::fs::write(target_rs, content).unwrap();
         }
+    }
+}
+
+fn copy_guest_code_to_crates() {
+    let wit_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("wit");
+    let expects = vec![
+        ("http_handler.rs", "crates/sdk/macro/src/http_handler.rs"),
+        ("http_service.rs", "crates/sdk/src/wit/http_service.rs"),
+    ];
+    // copy expects
+    for (source, target) in expects.iter() {
+        let source_path = wit_dir.join(Path::new(source));
+        let target_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join(target);
+        std::fs::copy(source_path, target_path).unwrap();
     }
 }
